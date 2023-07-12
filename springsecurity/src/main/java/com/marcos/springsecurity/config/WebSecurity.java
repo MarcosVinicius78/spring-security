5270b457b03e1fee84eb607da2a9a98004660d18
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2023-07-12 10:50:07 -0300

    autenticação padrão via banco de dados com o jdbc

--- springsecurity/src/main/java/com/marcos/springsecurity/config/WebSecurity.java
+++ springsecurity/src/main/java/com/marcos/springsecurity/config/WebSecurity.java
@@ -1,5 +1,7 @@
 package com.marcos.springsecurity.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,9 +9,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,25 +31,36 @@ public class WebSecurity {
         return http.build();
     }
 
+    /*nesse bean estamos fazendo a autenticação via banco de dados
+      essa autenticação tem que ser padrão do spring, então em minha tabela os nome dos atributos
+      devem estar como o spring quer, tabela user, atributos username, password e enabled, com isso ele faz a autenticação
+    */
     @Bean
-    public InMemoryUserDetailsManager userDetails() {
-
-        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
-        UserDetails user = User.withUsername("marcos")
-                .password("{noop}admin")
-                .authorities("USER")
-                .build();
-
-        UserDetails admin = User.withUsername("admin")
-                .password("admin")
-                .authorities("ADMIN")
-                .build();
-
-        inMemoryUserDetailsManager.createUser(user);
-        inMemoryUserDetailsManager.createUser(admin);
-
-        return inMemoryUserDetailsManager;
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
+    
+
+    //nesse bean estamos fazendo autenticação via memoria
+    // @Bean
+    // public InMemoryUserDetailsManager userDetails() {
+
+    //     InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+    //     UserDetails user = User.withUsername("marcos")
+    //             .password("{noop}admin")
+    //             .authorities("USER")
+    //             .build();
+
+    //     UserDetails admin = User.withUsername("admin")
+    //             .password("admin")
+    //             .authorities("ADMIN")
+    //             .build();
+
+    //     inMemoryUserDetailsManager.createUser(user);
+    //     inMemoryUserDetailsManager.createUser(admin);
+
+    //     return inMemoryUserDetailsManager;
+    // }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
